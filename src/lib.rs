5424238c29c84eba265cb644e4042cc74a25a27d
Dániel Buga <bugadani@gmail.com>
2023-10-16 20:22:04 +0200

    Temporarily readd #![feature(impl_trait_projections)]

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(not(test), no_std)]
-#![allow(unknown_lints, async_fn_in_trait)]
-#![allow(stable_features)]
+#![feature(impl_trait_projections)]
 #![feature(async_fn_in_trait)]
+#![allow(stable_features, unknown_lints, async_fn_in_trait)]
 
 #[cfg(feature = "async")]
 pub mod asynch;
