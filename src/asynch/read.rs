0920f14f4807168c05b440f75ebd1f0ad8cd5277
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-10-20 16:17:42 +0200

    Implement BufRead
    Closes #2

--- src/asynch/read.rs
+++ src/asynch/read.rs
@@ -1,4 +1,4 @@
-use embedded_io_async::{Read, Write};
+use embedded_io_async::{BufRead, Read, Write};
 
 /// A buffered [`Read`]
 ///
@@ -58,11 +58,27 @@ impl<T: Read> Read for BufferedRead<'_, T> {
             self.available -= len;
         } else {
             // The buffer is drained
-            self.offset = 0;
             self.available = 0;
         }
 
         Ok(len)
+    }
+}
+
+impl<T: Read> BufRead for BufferedRead<'_, T> {
+    async fn fill_buf(&mut self) -> Result<&[u8], Self::Error> {
+        if self.available == 0 {
+            self.offset = 0;
+            self.available = self.inner.read(self.buf).await?;
+        }
+
+        Ok(&self.buf[self.offset..self.offset + self.available])
+    }
+
+    fn consume(&mut self, amt: usize) {
+        assert!(amt <= self.available);
+        self.offset += amt;
+        self.available -= amt;
     }
 }
 
@@ -90,7 +106,7 @@ mod tests {
 
         let mut read_buf = [0; 8];
         assert_eq!(4, buffered.read(&mut read_buf).await.unwrap());
-        assert_eq!(0, buffered.offset);
+        assert_eq!(4, buffered.offset);
         assert_eq!(0, buffered.available);
         assert_eq!(&[5, 6, 7, 8], &read_buf[..4]);
     }
@@ -106,5 +122,38 @@ mod tests {
         assert_eq!(0, buffered.offset);
         assert_eq!(0, buffered.available);
         assert_eq!(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], read_buf.as_slice());
+    }
+
+    #[tokio::test]
+    async fn can_buf_read() {
+        let inner = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let mut buf = [0; 8];
+        let mut buffered = BufferedRead::new(inner.as_slice(), &mut buf);
+        assert_eq!(0, buffered.offset);
+        assert_eq!(0, buffered.available);
+
+        assert_eq!(
+            &[1, 2, 3, 4, 5, 6, 7, 8],
+            buffered.fill_buf().await.unwrap()
+        );
+        assert_eq!(0, buffered.offset);
+        assert_eq!(8, buffered.available);
+
+        buffered.consume(2);
+        assert_eq!(2, buffered.offset);
+        assert_eq!(6, buffered.available);
+        assert_eq!(&[3, 4, 5, 6, 7, 8], buffered.fill_buf().await.unwrap());
+
+        buffered.consume(6);
+        assert_eq!(8, buffered.offset);
+        assert_eq!(0, buffered.available);
+
+        assert_eq!(&[9, 10], buffered.fill_buf().await.unwrap());
+        assert_eq!(0, buffered.offset);
+        assert_eq!(2, buffered.available);
+
+        buffered.consume(2);
+        assert_eq!(2, buffered.offset);
+        assert_eq!(0, buffered.available);
     }
 }
