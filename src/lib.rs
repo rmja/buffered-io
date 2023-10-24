f4cff8d4b239abc03b1c6a2688e55ca0742c22d7
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-10-24 11:07:14 +0200

    For now, support older toolchain

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 #![cfg_attr(not(test), no_std)]
+#![allow(unknown_lints, async_fn_in_trait)]
+#![allow(stable_features)]
+#![feature(async_fn_in_trait)]
 
 #[cfg(feature = "async")]
 pub mod asynch;
